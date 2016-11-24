8adf491d8272f609607ab0f7ac4ecc1a511946f3
dblock <dblock@dblock.org>
2016-11-24 09:13:54 -0500

    Added help and stop intents.

--- test/functions/artsy/test-launch.js
+++ test/functions/artsy/test-launch.js
@@ -10,6 +10,7 @@ describe('artsy alexa', function() {
                 var data = JSON.parse(res.text);
                 expect(data.response.outputSpeech.type).to.equal('SSML')
                 expect(data.response.outputSpeech.ssml).to.equal('<speak>Welcome to Artsy! Ask me about an artist.</speak>')
+                expect(data.response.reprompt.outputSpeech.ssml).to.equal('<speak>Ask me about an artist. Say help if you need help or exit any time to exit.</speak>')
                 done();
             });
     });
