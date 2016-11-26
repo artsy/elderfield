fbf97fe13c4ec1e1256ff044a2177a1eac427a59
dblock <dblock@dblock.org>
2016-11-26 12:08:31 -0500

    The help prompt must end with a question for users or prompt the user to speak and leave the session open to receive a response.

--- test/functions/artsy/test-launch.js
+++ test/functions/artsy/test-launch.js
@@ -10,7 +10,7 @@ describe('artsy alexa', function() {
                 var data = JSON.parse(res.text);
                 expect(data.response.outputSpeech.type).to.equal('SSML')
                 expect(data.response.outputSpeech.ssml).to.equal('<speak>Welcome to Artsy! Ask me about an artist.</speak>')
-                expect(data.response.reprompt.outputSpeech.ssml).to.equal('<speak>Ask me about an artist. Say help if you need help or exit any time to exit.</speak>')
+                expect(data.response.reprompt.outputSpeech.ssml).to.equal('<speak>Say help if you need help or exit any time to exit. What artist would you like to hear about?</speak>')
                 done();
             });
     });
